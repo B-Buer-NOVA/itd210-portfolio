3916989443a65fd185379870d8d25deab5f9ba29
Berry Buer <aib83013@email.vccs.edu>
2026-04-26 21:02:28 -0400

    Updated navigation and added contact.html

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,13 +1,25 @@
 /* JavaScript for Nav Toggle */
 
-const navButton = document.querySelector(".mobile-nav");
-const navMenu = document.querySelector("#menu-drawer");
-const backButton = document.querySelector("#back");
+const navButton = document.querySelector(".mobile-nav"); // check for click
+const navMenu = document.querySelector("#menu-drawer"); // actual drawer
+const backButton = document.querySelector("#back"); // closes drawer
+const galleries = document.querySelector("#gallery-menu"); // check for click
+const galleryDrop = document.querySelector("#galleries-drop") // dropdown menu
 
 navButton.addEventListener("click", function () {
-    navMenu.className = 'open-drawer';
+    navMenu.className = 'open-drawer'; // class added that opens drawer
 })
 
 backButton.addEventListener("click", function () {
-    navMenu.className = '.';
+    navMenu.className = '.'; // class name removed
+})
+
+galleries.addEventListener("click", function () {
+    if (galleryDrop.style.display !== 'flex') {
+        galleryDrop.style.display = 'flex';
+    } else {
+        galleryDrop.style.display = 'none';
+    };
+    // if the gallery menu isn't displayed as flex, it's is flexed
+    // and if the gallery is flex, its display is set to non
 })
--- js/navigation.js
+++ js/navigation.js
@@ -2,38 +2,49 @@
     Updated navigation, so i don't have to update each page
     every time i add a new page
 */
-// basic 
-const navContent = document.getElementById("navigation");
-const navigation = document.getElementsByTagName("nav"); // where to insert
 
-// variables
+// basic variables for creation and sending to HTML
+const navContent = document.getElementById("navigation");
 const ul = document.createElement("ul");
 const linksArray = ['<a href="index.html">Home Page</a>',
     '<a href="about.html">About Page</a>', 
-    '<a href="crochet.html">Crochet</a>', 
-    '<a href="digital_art.html">Digital Art</a>', 
-    '<a href="traditional_art.html">Traditional Art</a>', 
     '<a href="data.html">Data Demo</a>', 
-    '<a href="media.html">Media</a>' ];
+    '<a href="media.html">Media</a>', 
+    '<a href="contact.html">Contact</a>'];
+const gallery = ['<a href="crochet.html">Crochet</a>', 
+    '<a href="digital_art.html">Digital Art</a>', 
+    '<a href="traditional_art.html">Traditional Art</a>']
 // linksArray holds links of every webpage
 
-// specific buttons and links
-const homePage = document.createElement("li"); // home page link displayed at all times
+// special buttons and links that hav specific tasks
+const homePage = document.createElement("li"); // home page link displayed at all times (no classes added)
 const menuDrawer = document.createElement("li"); // menuDrawer overlay, holds .links
 const links = document.createElement("div"); // holds links
 const navToggle = document.createElement("button"); // appears on smaller screens, summons .links
+const galleryMenu = document.createElement("li"); // holds and toggles (other js file) dropdown menu for three galleries to declutter header navigation
+const dropdown = document.createElement("div"); // holds gallery links
 const back = '<button id="back">&#8592; Back</button>';
 
-// application of class, id, and content to specific buttons 
-navToggle.id = 'nav-toggle';
+// set up of class, id, and content to special nav functions
+// mobile nav toggle creation
 navToggle.classList.add('mobile-nav');
+navToggle.id = 'nav-toggle';
 navToggle.innerHTML = "&#9776;";
+// gallery menu creation
+galleryMenu.id = 'gallery-menu';
+galleryMenu.classList.add("desktop");
+galleryMenu.tabIndex = "0";
+galleryMenu.style.textDecoration = 'underline';
+galleryMenu.innerHTML += 'galleries';
+// id for styling added to dropdown
+dropdown.id = 'galleries-drop';
+// menu drawer and links creation
 menuDrawer.id = 'menu-drawer';
 links.className = 'links';
 links.innerHTML += back; // back button at the top
 
-// desktop additions
-homePage.innerHTML = linksArray[0]; // home page / index.html
+// creation of navigation bar
+homePage.innerHTML = linksArray[0]; // home page or index.html
 ul.appendChild(homePage); // first link in nav
 
 for (let i = 1; i < linksArray.length; i++) {
@@ -46,13 +57,25 @@ for (let i = 1; i < linksArray.length; i++) {
     ul.appendChild(li);
 }
 
-ul.appendChild(navToggle)
-
-// links in links div
-for (let i = 1; linksArray[i]; i++) {
-    links.innerHTML += linksArray[i];
+for (let i = 0; i < gallery.length; i++) {
+    // adding galleries to dropdown menu
+    dropdown.innerHTML += gallery[i];
 }
 
-menuDrawer.appendChild(links); // menuDrawer has contents
+galleryMenu.appendChild(dropdown); // dropdown menu appended to the galleries link
+ul.appendChild(galleryMenu); // galleries link appended to ul
+ul.appendChild(navToggle); // navToggle appended
+
+// links for menuDrawer
+for (let i = 1; linksArray[i]; i++) {
+    // appending links
+    links.innerHTML += linksArray[i];
+}
+for (let i = 0; gallery[i]; i++) {
+    // appending galleries to the end of the list
+    links.innerHTML += gallery[i];
+}
+
+menuDrawer.appendChild(links); // menuDrawer's contents appended
 ul.appendChild(menuDrawer); // appended to ul
-navContent.appendChild(ul); // ul is sent to nav via id
+navContent.appendChild(ul); // ul is finished and sent to av via id
